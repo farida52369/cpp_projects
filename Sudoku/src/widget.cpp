b830dc8996d9aa0279b6b09bf43cac11f20bc171
fareedaragab <66780788+farida52369@users.noreply.github.com>
2022-05-05 22:01:23 +0200

    Sudoku game using Qt5

--- Sudoku/src/widget.cpp
+++ Sudoku/src/widget.cpp
@@ -20,7 +20,8 @@ Widget::Widget(QWidget *parent)
     // ComboBox Values __ To Start New Game
     // According to chosen Level
     QStringList levels = {"Easy", "Meduim", "Hard"};
-    foreach (QString s, levels) {
+    foreach (QString s, levels)
+    {
         ui->comboBox->addItem(s);
     }
 }
@@ -35,8 +36,10 @@ void Widget::on_comboBox_currentIndexChanged(int index)
     ui->board->clear();
 
     // Initialize The array in the begining of every New Game
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             board[i][j] = 0;
         }
     }
@@ -46,26 +49,31 @@ void Widget::on_comboBox_currentIndexChanged(int index)
 
     // Initialize The Table Widget with empty "" -> board[i][j] = 0
     // with The value -> board[i][j] != 0
-    for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                    if (board[i][j] != 0){
-                        QTableWidgetItem* item = ui->board->item(i, j);
-                        if(!item) {
-                            item = new QTableWidgetItem();
-                            ui->board->setItem(i, j, item);
-                        }
-                        item->setText(QString::number(board[i][j]));
-                        item->setFlags(item->flags() & ~Qt::ItemIsEditable);
-                    }
-                    temp_board[i][j] = board[i][j];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (board[i][j] != 0)
+            {
+                QTableWidgetItem* item = ui->board->item(i, j);
+                if(!item)
+                {
+                    item = new QTableWidgetItem();
+                    ui->board->setItem(i, j, item);
+                }
+                item->setText(QString::number(board[i][j]));
+                item->setFlags(item->flags() & ~Qt::ItemIsEditable);
             }
-      }
+            temp_board[i][j] = board[i][j];
+        }
+    }
 }
 
 void Widget::on_undo_clicked()
 {
     QTableWidgetItem* item = ui->board->currentItem();
-    if(!item) {
+    if(!item)
+    {
         item = new QTableWidgetItem();
         ui->board->setCurrentItem(item);
     }
@@ -76,28 +84,36 @@ void Widget::on_undo_clicked()
 void Widget::on_solveTheGame_clicked()
 {
     GenerateSudoku::solve(temp_board);
-    for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                QTableWidgetItem* item = ui->board->item(i, j);
-                if(!item) {
-                    item = new QTableWidgetItem();
-                    ui->board->setItem(i, j, item);
-                }
-                item->setText(QString::number(temp_board[i][j]));
-                item->setFlags(item->flags() & ~Qt::ItemIsEditable);
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            QTableWidgetItem* item = ui->board->item(i, j);
+            if(!item)
+            {
+                item = new QTableWidgetItem();
+                ui->board->setItem(i, j, item);
             }
-      }
+            item->setText(QString::number(temp_board[i][j]));
+            item->setFlags(item->flags() & ~Qt::ItemIsEditable);
+        }
+    }
 }
 
 void Widget::on_board_cellChanged(int row, int column)
 {
-        QTableWidgetItem* item = ui->board->item(row, column);
-        if (!((item->text().toInt() > 0 && item->text().toInt() < 10) || item->text() == ""))  {
-            if(!item) {
-                item = new QTableWidgetItem();
-                ui->board->setItem(row, column, item);
-            }
-            item->setText("");
-            ui->label->setText("Warning: add only numbers from [1:9]");
+    QTableWidgetItem* item = ui->board->item(row, column);
+    if (!((item->text().toInt() > 0 && item->text().toInt() < 10) || item->text() == ""))
+    {
+        if(!item)
+        {
+            item = new QTableWidgetItem();
+            ui->board->setItem(row, column, item);
         }
+        item->setText("");
+        ui->label->setText("Warning: add only numbers from [1:9]");
+    }
 }
+
+// I'M NOT VERY GOOD AT QT5 BUT TO DEMONSTRATE MY KNOWLEDGE
+// IN OOP CPP :)))
